8fbc7e00dfec473f63eb58d64b3e98e56bc1ab2b
techwithguga <guramxatiashvili@hotmail.com>
2024-02-07 13:34:27 +0000

    break down the problem into small chunks

---
+++ main.py
@@ -0,0 +1,21 @@
+#display art
+
+# generate a random account from the game data
+
+# format the account data into printable format 
+
+#ask user for a guess.
+
+#check if user is correct
+## get the follower count of each account
+## use if statement to compare and check if user is correct
+
+#give user feedback on their guess.
+
+#keep score +1 if user guess is correct. End game if wrong and display score
+
+#make game repeatable if user is right. recursive game()
+
+#make account position B become the next account at position A
+
+#clear the terminal screen between rounds.
