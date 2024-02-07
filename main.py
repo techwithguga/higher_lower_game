0066c9a9931d6af79f27a1bd4fe7a05ed07cb5c1
techwithguga <guramxatiashvili@hotmail.com>
2024-02-07 14:16:15 +0000

    add UI touch, print vs between comparisons

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from art import logo
+from art import logo, vs
 from game_data import data
 import random
 
@@ -22,10 +22,12 @@ print(account_b)
 if account_a == account_b:
     account_b = random.choice(data)
 
-print(f"Compare A :{format_data(account_a)}")
-print(f"Compare B :{format_data(account_b)}")
+print(f"Compare A : {format_data(account_a)}")
+print(vs)
+print(f"Against B : {format_data(account_b)}")
 
 #ask user for a guess.
+guess = input("Who has more followers ? Type 'A' or 'B': ").lower()
 
 #check if user is correct
 ## get the follower count of each account
